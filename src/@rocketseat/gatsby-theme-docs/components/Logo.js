661830cba67845ea86caa1b398589bd3cfdc9344
robjwa20 <robert.j.w.allen@gmail.com>
2021-04-24 01:24:19 +0100

    png 2

--- src/@rocketseat/gatsby-theme-docs/components/Logo.js
+++ src/@rocketseat/gatsby-theme-docs/components/Logo.js
@@ -1,7 +1,7 @@
 import React from 'react';
 
 // Path to the logo file on your project
-import rocketseatLogo from 'assets/logo.png';
+import godssandboxLogo from 'assets/logo.png';
 
 const Logo = () => (
   <img src={godssandboxLogo} alt="Gods Sandbox Logo" style={{ width: 180 }} />
